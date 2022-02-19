58fe853459015587cc587650348c7104122077a2
SeanOMik <seanomik@gmail.com>
2022-02-19 13:18:59 -0500

    Try to fix CI CI part 2 Update Action-SpigotMC Create compileSpigotVersions.sh CI p3 ci p4 ci p5 ci p6 ci p7 ci p8 Auto stash before merge of "master" and "origin/master" ci p9
    ci p10
    ci p11

---
+++ compileSpigotVersions.sh
@@ -0,0 +1,9 @@
+java -jar ./BuildTools.jar --rev 1.14.4 --compile-if-changed
+java -jar ./BuildTools.jar --rev 1.15 --compile-if-changed
+java -jar ./BuildTools.jar --rev 1.16.1 --compile-if-changed
+java -jar ./BuildTools.jar --rev 1.16.3 --compile-if-changed
+java -jar ./BuildTools.jar --rev 1.16.5 --compile-if-changed
+java -jar ./BuildTools.jar --rev 1.17 --remapped remapped-mojang --compile-if-changed
+java -jar ./BuildTools.jar --rev 1.17.1 --remapped remapped-mojang --compile-if-changed
+java -jar ./BuildTools.jar --rev 1.18 --remapped remapped-mojang --compile-if-changed
+java -jar ./BuildTools.jar --rev 1.18.1 --remapped remapped-mojang --compile-if-changed
