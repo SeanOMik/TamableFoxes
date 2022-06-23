9a623d9bc3b78eec32435594d1fe8658baf842f1
Matthew Shipman <shipman.matt@gmail.com>
2022-06-23 16:35:05 +0000

    Update for 1.19

---
+++ 1_19_R1/src/main/java/net/seanomik/tamablefoxes/versions/version_1_19_R1/NMSInterface_1_19_R1.java
@@ -0,0 +1,54 @@
+package net.seanomik.tamablefoxes.versions.version_1_19_R1;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Fox;
+import net.seanomik.tamablefoxes.util.FieldHelper;
+import net.seanomik.tamablefoxes.util.NMSInterface;
+import net.seanomik.tamablefoxes.util.io.Config;
+import net.seanomik.tamablefoxes.util.io.LanguageConfig;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_19_R1.entity.CraftEntity;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Field;
+import java.util.UUID;
+
+public class NMSInterface_1_19_R1 implements NMSInterface {
+    @Override
+    public void registerCustomFoxEntity() {
+        try { // Replace the fox entity
+            Field field = EntityType.FOX.getClass().getDeclaredField("bs"); // bs = factory
+            FieldHelper.setFieldUsingUnsafe(field, EntityType.FOX, (EntityType.EntityFactory<Fox>) EntityTamableFox::new);
+            Bukkit.getServer().getConsoleSender().sendMessage(Config.getPrefix() + ChatColor.GREEN + LanguageConfig.getSuccessReplaced());
+        } catch (Exception e) {
+            Bukkit.getServer().getConsoleSender().sendMessage(Config.getPrefix() + ChatColor.RED + LanguageConfig.getFailureReplace());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void spawnTamableFox(Location loc, FoxType type) {
+        EntityTamableFox tamableFox = (EntityTamableFox) ((CraftEntity) loc.getWorld().spawnEntity(loc, org.bukkit.entity.EntityType.FOX)).getHandle();
+        tamableFox.setFoxType((type == FoxType.RED) ? Fox.Type.RED : Fox.Type.SNOW);
+    }
+
+    @Override
+    public void changeFoxOwner(org.bukkit.entity.Fox fox, Player newOwner) {
+        EntityTamableFox tamableFox = (EntityTamableFox) ((CraftEntity) fox).getHandle();
+        tamableFox.setOwnerUUID(newOwner.getUniqueId());
+    }
+
+    @Override
+    public UUID getFoxOwner(org.bukkit.entity.Fox fox) {
+        EntityTamableFox tamableFox = (EntityTamableFox) ((CraftEntity) fox).getHandle();
+        return tamableFox.getOwnerUUID();
+    }
+
+    @Override
+    public void renameFox(org.bukkit.entity.Fox fox, Player player) {
+        EntityTamableFox tamableFox = (EntityTamableFox) ((CraftEntity) fox).getHandle();
+        tamableFox.rename(player);
+    }
+}
--- Plugin/src/main/java/net/seanomik/tamablefoxes/TamableFoxes.java
+++ Plugin/src/main/java/net/seanomik/tamablefoxes/TamableFoxes.java
@@ -16,6 +16,7 @@ import net.seanomik.tamablefoxes.util.io.LanguageConfig;
 import net.seanomik.tamablefoxes.versions.version_1_18_1_R1.NMSInterface_1_18_1_R1;
 import net.seanomik.tamablefoxes.versions.version_1_18_R1.NMSInterface_1_18_R1;
 import net.seanomik.tamablefoxes.versions.version_1_18_R2.NMSInterface_1_18_R2;
+import net.seanomik.tamablefoxes.versions.version_1_19_R1.NMSInterface_1_19_R1;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.*;
 import org.bukkit.event.Listener;
@@ -72,6 +73,8 @@ public final class TamableFoxes extends JavaPlugin implements Listener {
             nmsInterface = new NMSInterface_1_18_1_R1();
         } else if (versionDouble == 18.2D) {
             nmsInterface = new NMSInterface_1_18_R2();
+        } else if (versionDouble == 19D) {
+            nmsInterface = new NMSInterface_1_19_R1();
         } else {
             Bukkit.getServer().getConsoleSender().sendMessage(Config.getPrefix() + ChatColor.RED + LanguageConfig.getUnsupportedMCVersionRegister());
             Bukkit.getServer().getConsoleSender().sendMessage(Config.getPrefix() + ChatColor.RED + "You're trying to run MC version " + specificVersion + " which is not supported!");
